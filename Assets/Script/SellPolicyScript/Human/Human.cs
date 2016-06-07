e7d2e493d8d5830ff2dad109cc81f0a395305a5c
BaikSeongHyun <red96146@naver.com>
2016-06-08 00:33:12 +0900

    집작업
    사람 기본 형태 잡고 준비함
    올 ㅋ

---
+++ Assets/Script/SellPolicyScript/Human/Human.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Human : MonoBehaviour
+{
+	//simple data field
+	public float disposition;
+	public State state
+	//complex data field
+
+	//enum field
+	public enum State
+	{
+		Customer,
+		Thief
+	};
+
+	//initialize this script
+	void Start ()
+	{
+	
+	}
+
+}
